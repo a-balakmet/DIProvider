e4a0074a18fc895695d6afebdf54ae5bcfb67d55
Andrey <61378523+a-balakmet@users.noreply.github.com>
2023-08-02 13:15:47 +0600

    Initial commit

---
+++ Classes/DIContainer.swift
@@ -0,0 +1,30 @@
+//
+//  DIContainer.swift
+//  DIProvider
+//
+//  Created by Andrey Alexandrov on 02.08.2023.
+//
+
+import Foundation
+
+protocol DIContainerProtocol {
+  func register<Component>(type: Component.Type, component: Any)
+  func resolve<Component>(type: Component.Type) -> Component?
+}
+
+final class DIContainer: DIContainerProtocol {
+    
+  static let shared = DIContainer()
+    
+  private init() {}
+    
+  var components: [String: Any] = [:]
+
+  func register<Component>(type: Component.Type, component: Any) {
+    components["\(type)"] = component
+  }
+
+  func resolve<Component>(type: Component.Type) -> Component? {
+    return components["\(type)"] as? Component
+  }
+}
